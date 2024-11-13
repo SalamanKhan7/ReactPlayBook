bc6b92144c37fabd9f472e95e54da823d60bf3d4
salaman <sk2562446@gmail.com>
2024-11-13 09:51:01 +0530

    initial react

---
+++ dist/index.03652dde.js
@@ -0,0 +1,2 @@
+const e=React.createElement("h1",{id:"heading",abc:"xyz"},"Hello React from React");ReactDOM.createRoot(document.getElementById("root")).render(e);
+//# sourceMappingURL=index.03652dde.js.map
