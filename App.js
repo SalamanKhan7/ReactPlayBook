e4450012d537e2a717e3eae8078b8b892291933f
salaman <sk2562446@gmail.com>
2024-11-13 10:07:36 +0530

    revision

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement(
   "h1",
   { id: "heading", abc: "xyz" },
   "Hello React from React"
 );
+
+/** JSX **/
+const title = <h1>React from JSX</h1>; //title is a object and work as title
+/**
+ * component composition
+ */
+const HeadingCom = () => {
+  return <h2>{title}react from component function</h2>;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+/**react dom work as a bridge between react and browser */
+// root.render(heading);
+// root.render(title);
+// root.render(HeadingCom());
+root.render(<HeadingCom />);
